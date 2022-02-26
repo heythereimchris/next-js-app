0048391e578221d0f72d72bbcaef01a92c7e5979
“heythereimchris” <chrissanfilippo10@gmail.com>
2022-02-26 09:47:05 -0800

    app setup

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8f45416105ebceed"; }
+/******/ 		__webpack_require__.h = function() { return "12ab84eadf38af86"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Next JS
+          Next JS Apps
         </h1>
 
         <p className={styles.description}>
